5593f20be982381d612cd728dad845323d65a416
abcdefghachi <nguyenhachi793@gmail.com>
2024-03-22 13:49:04 +0700

    initial commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,15 @@ export default function Login() {
     const clientId = "d0300acf729249f999aaf25986a23d39"
     const redirectUrl = "http://localhost:3000/"
     const apiUrl = "https://accounts.spotify.com/authorize"
-    const scope = ['user-read-email','user-read-private']
+    const scope = ["user-read-email",
+    "user-read-private" , 
+    "user-read-playback-state",
+    "user-modify-playback-state",
+    "user-read-currently-playing" , 
+    "user-read-playback-position",
+    "user-top-read",
+    "user-read-recently-played"]
+    window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join( " ")}&response_type=tolen&show_daialog=true`
   }
   return (
     <Container className="d-flex flex-column vh-100 vw-100">
